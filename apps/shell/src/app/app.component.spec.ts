5b4ae4768c9635d7928db71ad32c3c7bc3df7cd8
Halisson Brancalhao <halisson@brancalhao.com.br>
2025-09-24 00:17:48 -0300

    refactor: replace NxWelcome component with AppComponent in routing and tests

--- apps/shell/src/app/app.component.spec.ts
+++ apps/shell/src/app/app.component.spec.ts
@@ -1,15 +1,13 @@
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { NxWelcome } from './nx-welcome';
-import { Router, RouterModule } from '@angular/router';
+import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([{ path: '', component: NxWelcome }]),
+        RouterModule.forRoot([{ path: '', component: AppComponent }]),
         AppComponent,
-        NxWelcome,
       ],
     }).compileComponents();
   });
@@ -25,16 +23,4 @@ describe('App', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('shell');
   });
-
-  it('should render title', fakeAsync(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const router = TestBed.inject(Router);
-    fixture.ngZone?.run(() => router.navigate(['']));
-    tick();
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome shell'
-    );
-  }));
 });
