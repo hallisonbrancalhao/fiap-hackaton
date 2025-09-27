8c3ebbc2c91a571b295d15d1a933c0d67ff38166
Halisson Brancalhao <hallison.oliveira@4asset.com.br>
2025-09-27 14:43:54 -0300

    fix: changes husky pre-commit

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -5,10 +5,8 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   public title = 'shell';
-
-  text = 'test husky';
 }
