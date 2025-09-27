e1e5d463871969f843495054c04f55e04cbd71fb
Halisson Brancalhao <hallison.oliveira@4asset.com.br>
2025-09-27 17:12:04 -0300

    feat: enhance app layout with welcome message and PrimeNG button integration

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Button } from 'primeng/button';
 
 @Component({
-  imports: [RouterModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	imports: [RouterModule, Button],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'shell';
+	public title = 'shell';
 }
